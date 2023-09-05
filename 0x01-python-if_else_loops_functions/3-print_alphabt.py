05dcfa84542fc22878fa32b8b63de190c4b55147
imadovetch <ibouderoua63@gmail.com>
2023-09-05 16:48:16 +0100

    first 10 task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for ascii_value in range(ord('a'), ord('z') + 1):
-    if chr(ascii_value) == 'e' or ascii_value == 'q':
-        continue
-    print(chr(ascii_value), end='')
-	
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
