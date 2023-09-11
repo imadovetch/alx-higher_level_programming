461fcfce1167d7e7f9574003a9bb62c084d95db6
imadovetch <ibouderoua63@gmail.com>
2023-09-11 13:03:07 +0100

    deja fais

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    dup = [x for x in my_list]
-    dup[idx] = element
+
+    copy = [x for x in my_list]
+    copy[idx] = element
     return (copy)
