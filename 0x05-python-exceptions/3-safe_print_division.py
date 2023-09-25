ed63a9f0074ecdae6f7c7d2bf595dc7ef1aceb9f
imadovetch <ibouderoua63@gmail.com>
2023-09-25 15:19:40 +0100

    all mandatory tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
         print("Inside result: {:.1f}".format(result))
         return result
     except ZeroDivisionError:
-        print("Division by zero is not allowed.")
+        print("Inside result: {}".format(None))
         return None
