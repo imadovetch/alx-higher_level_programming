2ea1f4b58d8881a3ac2ef43c8abdea5672a318b4
imadovetch <ibouderoua63@gmail.com>
2023-09-25 13:37:26 +0100

    first push 3 tasks

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,6 @@
+def safe_print_integer(x):
+    try:
+        print("{:d}".format(x))
+    except (TypeError, ValueError):
+        return False
+    return True
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    tot = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            tot += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return tot
